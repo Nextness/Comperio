dcd07df8b43f19bc6e942882504e1e6e26a6b029
Natan Menegasse <natan.menegasse@hotmail.com>
2023-02-19 22:14:39 -0300

    Updating Onboarding Pages (#6)

--- app/src/main/java/com/research/comperio/ui/theme/Type.kt
+++ app/src/main/java/com/research/comperio/ui/theme/Type.kt
@@ -1,10 +1,18 @@
 package com.research.comperio.ui.theme
 
+import androidx.compose.material3.Text
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.research.comperio.R
+
+val Satoshi = FontFamily(
+    Font(R.font.satoshi_regular),
+    Font(R.font.satoshi_bold, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
